395e074901ccedc3c2cead0b808bf6f19039b504
Lienioos <Lien.larionov@yandex.ru>
2024-06-19 22:03:45 +0700

    1
    Это база (Бд и подключение к админ панели)

---
+++ university/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    student_id = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    subject_code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.student} - {self.subject}: {self.grade}"
